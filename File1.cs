decf49512fabf981157328a00c3aaaabb161056d
Christopher Rhodes <chrhodes@cox.net>
2023-06-10 23:09:16 -0700

    Added some comments

--- File1.cs
+++ File1.cs
@@ -1,15 +1,15 @@
 // File1
 void method1()
 {
-
+    // method1 code goes here
 }
 
 void method2()
 {
-    
+    // method2 code goes here
 }
 
 void method5()
 {
-    
+    // method5 code goes here    
 }
--- File2.cs
+++ File2.cs
@@ -1,11 +1,11 @@
 // File2
 void method3()
 {
-
+    // method3 code goes here
 }
 
 void method4()
 {
-    
+    // method4 code goes here    
 }
 
