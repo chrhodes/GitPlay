e754daaa528aa696d187cfacf7034f21e351b6b3
Christopher Rhodes <chrhodes@cox.net>
2023-06-10 23:14:13 -0700

    Added comments

--- File1.cs
+++ File1.cs
@@ -1,10 +1,10 @@
 // File1
 void method1()
 {
-
+    // Add code here
 }
 
 void method2()
 {
-    
+    // Add code here    
 }
--- File2.cs
+++ File2.cs
@@ -1,15 +1,15 @@
 // File2
 void method3()
 {
-
+    // Add code here
 }
 
 void method4()
 {
-    
+    // Add code here   
 }
 
 void method6()
 {
-    
+    // Add code here
 }
